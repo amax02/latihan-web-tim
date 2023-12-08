e675ac4ebbb62e68bcd501099853de83afa8836f
zamzam <hayabusa2478@gmail.com>
2023-12-08 09:26:25 +0700

    up login

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $conn = mysqli_connect("localhost", "root", "", "testing");
+    $conn = mysqli_connect($localhost, $root, "", $testing);
 
     if (!$conn) {
         die("Koneksi gagal: " . mysqli_connect_error());
